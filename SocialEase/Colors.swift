abd2fb328ac8ff4692f62eefc75a5d4b5c9fc6d2
ykay <ykay@users.noreply.github.com>
2015-10-18 13:22:38 -0700

    Update Colors base colors

--- SocialEase/Colors.swift
+++ SocialEase/Colors.swift
@@ -9,10 +9,16 @@
 import UIKit
 
 extension UIColor {
-  static func sea_primaryTextColor() -> UIColor {
-    return UIColor.whiteColor()
-  }
-  static func sea_primaryBackgroundColor() -> UIColor {
-    return UIColor(red: 0.0/255.0, green: 114.0/255.0, blue: 187.0/255.0, alpha: 1.0)
-  }
+    static func sea_primaryColor() -> UIColor {
+        return UIColor(red: 0.0/255.0, green: 114.0/255.0, blue: 187.0/255.0, alpha: 1.0)
+    }
+    static func sea_primaryBackgroundColor() -> UIColor {
+        return UIColor.whiteColor()
+    }
+    static func sea_primaryLightTextColor() -> UIColor {
+        return UIColor.whiteColor()
+    }
+    static func sea_primaryHighlightColor() -> UIColor {
+        return UIColor(red: 187.0/255.0, green: 73.0/255.0, blue: 0.0/255.0, alpha: 1.0)
+    }
 }
