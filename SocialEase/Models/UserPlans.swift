b6d1d179e53c1d0443f1e2c08984ba5ce231cac8
Amay Singhal <amaysinghal@live.com>
2015-10-24 10:10:58 -0700

    Refs #13: Move model files into models folder (on disk)

---
+++ SocialEase/Models/UserPlans.swift
@@ -0,0 +1,13 @@
+//
+//  UserPlans.swift
+//  SocialEase
+//
+//  Created by Amay Singhal on 10/24/15.
+//  Copyright © 2015 Yuichi Kuroda. All rights reserved.
+//
+
+import UIKit
+
+class UserPlans: NSObject {
+
+}
