dfeda96eebc069283a1c9cfa7c1de5857659d978
Amay Singhal <amaysinghal@live.com>
2015-10-25 20:31:03 -0700

    Refs #14: [User Story #3] Add blur function to ViewTransformationUtils.swift

--- SocialEase/ViewTransformationUtils.swift
+++ SocialEase/ViewTransformationUtils.swift
@@ -11,6 +11,8 @@ import UIKit
 
 class ViewTransformationUtils {
 
+    static let BlurViewTag = 1000
+
     class func convertViewToCircle(myView: UIView, borderColor: UIColor, borderWidth: Int) {
         myView.layer.masksToBounds = false
         myView.layer.cornerRadius = myView.frame.size.height/2
@@ -20,4 +22,17 @@ class ViewTransformationUtils {
         myView.layer.borderColor = borderColor.CGColor
     }
 
+    static func addBlurToView(view: UIView, frame: CGRect, style: UIBlurEffectStyle = .ExtraLight) {
+        let blurEffect: UIBlurEffect = UIBlurEffect(style: style)
+        let blurView = UIVisualEffectView(effect: blurEffect)
+        blurView.tag = BlurViewTag
+        blurView.frame = frame
+
+        // check if blur view is already added at index 0 by looking at view tag
+        // remove is required so that multiple blur views are not added to view
+        if view.subviews.count > 0 && view.subviews[0].tag == BlurViewTag {
+            view.subviews[0].removeFromSuperview()
+        }
+        view.insertSubview(blurView, atIndex: 0)
+    }
 }
